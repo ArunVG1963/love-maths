09dac7cf7a0713738ac521fa77aca30091c21ca6
Arun VenuGopal <ewo73h24@students.codeinstitute.net>
2024-11-06 10:23:27 +0000

    setup structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
 console.log("connected!")
+function runGame() {
+
+}
+function checkAnswer(){
+
+}
+function calculateCorrectAnswer(){
+
+}
+function incrementScore(){
+
+}
+function incrementWrongAnswer(){
+
+}
+function displayAddtionQuestion() {
+
+}
+function displaySubtractQuestion() {
+
+}
+function displayMultiplyQuestion() {
+
+}
+function displayDivisionQuestion() {
+    
+}
