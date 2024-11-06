0e70feecd3552b6cb465c30e10f7a084e183b2bc
Arun VenuGopal <ewo73h24@students.codeinstitute.net>
2024-11-06 12:31:58 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,18 +12,39 @@ document.addEventListener("DOMContentLoaded", function() {
          else
          {
             let gameType=this.getAttribute("data-type");
-            alert(`You clicked ${gameType.toUpperCase()}`);
+            runGame(gameType);
          }
         })
     }
+      runGame("addition");
+
 })
 /** The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
+
 let num1=Math.floor(Math.random()*25)+1;
 let num2=Math.floor(Math.random()*25)+1;
+
+if (gameType === "addition") {
+   displayAddtionQuestion(num1,num2);
+}
+ else {
+   alert(`Unknown game type: ${gameType}`);
+   throw `Unknown game type: ${gameType}. Aborting!`;
+    }   
+  
+if (gameType === "subtract") {
+   displaySubtractQuestion(num1,num2);
+}
+if (gameType === "multiply") {
+   displayMultiplyQuestion(num1,num2);
+}
+if (gameType === "division") {
+   displayDivisionQuestion(num1,num2);
+}
 
 }
 
@@ -39,8 +60,11 @@ function incrementScore(){
 function incrementWrongAnswer(){
 
 }
-function displayAddtionQuestion() {
-
+function displayAddtionQuestion(operand1, operand2) {
+   document.getElementById("operand1").textContent=operand1;
+   document.getElementById("operand2").textContent=operand2;
+   document.getElementById("operator").textContent="+";
+   
 }
 function displaySubtractQuestion() {
 
