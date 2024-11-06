5f451b10456c45da414b495858b80a67eb53651a
Arun VenuGopal <ewo73h24@students.codeinstitute.net>
2024-11-06 11:08:27 +0000

    Added EventListeners for DOM load and checking which button clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
 console.log("connected!")
+// Wait for the DOM to finish loading before the game
+// Get the buttone elements and add event listeners to them
+document.addEventListener("DOMContentLoaded", function() {
+ let buttons  = document.getElementsByTagName("button") ; 
+ console.log(buttons);
+    for (let button of buttons){
+        button.addEventListener("click",function(){
+         if(this.getAttribute("data-type") === "submit") {
+            alert(`You clicked ${this.getAttribute("data-type").toUpperCase()}!`)
+         }  
+         else
+         {
+            let gameType=this.getAttribute("data-type");
+            alert(`You clicked ${gameType.toUpperCase()}`);
+         }
+        })
+    }
+})
 function runGame() {
 
 }
@@ -24,5 +42,5 @@ function displayMultiplyQuestion() {
 
 }
 function displayDivisionQuestion() {
-    
+
 }
